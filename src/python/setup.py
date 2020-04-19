f31f203e2b20f2e249bd57a7368918570d93bb47
Bron Davies <bron.davies@aa.com>
2020-04-19 18:46:48 -0500

    Add python library

---
+++ src/python/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fttlib",
+    version="1.0.0",
+    author="Bron Davies",
+    author_email="bron@brontech.com",
+    description="A library of helper methods for your Python project to get mime types and general file category",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/brondavies/filetypetranslator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)

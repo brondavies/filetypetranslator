f511496580a276da2614dab308e087e369fc5c01
Bron Davies <bron@brontech.com>
2025-05-05 15:02:17 -0600

    Update Python packaging

--- src/python/deploy.cmd
+++ src/python/deploy.cmd
@@ -3,16 +3,17 @@ setlocal
 set path=%path%;C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64
 
 ::cleanup
-del /y dist\*.*
+del /Q dist\*.*
 
 ::Requirements
 ::python -m pip install --user --upgrade setuptools twine wheel
-
-python setup.py sdist bdist_wheel
+::python -m pip install --upgrade packaging
+py -m pip install --upgrade build
+py -m build
 ::These commands require the user name and password for your pypi accounts (test and prod are different accounts)
 ::TEST
 ::python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 ::PROD
-python -m twine upload dist/*
+py -m twine upload dist/*
 
 endlocal
